5eb03f58759f9de09fecc54cec3aee16f3574792
LoliPain <p2love.loli@gmail.com>
2023-09-22 10:32:26 +0400

    index template CBV processing

--- server/urls.py
+++ server/urls.py
@@ -18,7 +18,10 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from .views import IndexView
+
 urlpatterns = [
+    path("", IndexView.as_view(), name='Index'),
 ]
 
 urlpatterns.extend(static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
