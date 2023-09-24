00b6a2f469aab1619fc96c23c1f08b972f32275c
LoliPain <p2love.loli@gmail.com>
2023-09-24 07:03:55 +0400

    default values for decouple

--- server/settings.py
+++ server/settings.py
@@ -22,10 +22,10 @@ PROJECT_ROOT = BASE_DIR / 'server'
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("DJANGO_SECRET")
+SECRET_KEY = config("DJANGO_SECRET", default="")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DJANGO_DEBUG")
+DEBUG = config("DJANGO_DEBUG", default=False)
 
 
 # Application definition
