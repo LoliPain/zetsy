44a8a7afb38d57b1ecda3255bc9f68c5c1cd1194
LoliPain <p2love.loli@gmail.com>
2023-09-28 10:31:10 +0400

    refactor structure

---
+++ server/core/__init__.py
@@ -0,0 +1,7 @@
+from .config import CoreConfig
+from .views import IndexView
+
+__all__ = [
+    "CoreConfig",
+    "IndexView"
+]
--- server/urls.py
+++ server/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import IndexView
+from .core import IndexView
 
 urlpatterns = [
     path("", IndexView.as_view(), name='Index'),
