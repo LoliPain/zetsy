22bc7e2b2a1e14b78632d7e64e0ecab83e1a7e30
LoliPain <p2love.loli@gmail.com>
2023-09-28 11:32:20 +0400

    unnecessary static serving on dev

--- server/urls.py
+++ server/urls.py
@@ -15,12 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 
 urlpatterns = [
     path("", include("server.core.urls")),
 ]
-
-urlpatterns.extend(static(settings.STATIC_URL))
