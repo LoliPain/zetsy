237c05931151877009cd9c4ca1c7f7621dadf9a8
LoliPain <p2love.loli@gmail.com>
2023-09-29 17:15:42 +0400

    specify `CharField` max_length

--- server/models/uploader.py
+++ server/models/uploader.py
@@ -2,9 +2,9 @@ from django.db.models import Model, CharField, BooleanField
 
 
 class UploaderModel(Model):
-    uploader_token = CharField()
+    uploader_token = CharField(max_length=64)
     is_valid = BooleanField()
 
 
 class UploadedByModel(UploaderModel):
-    uploader_ip = CharField()
+    uploader_ip = CharField(max_length=64)
