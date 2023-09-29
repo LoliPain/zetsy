194b238f5c6a9248a4a9bb2d93c80434c27c132b
LoliPain <p2love.loli@gmail.com>
2023-09-29 17:19:58 +0400

    matching serializer for content-type

--- server/api/views/upload.py
+++ server/api/views/upload.py
@@ -9,6 +9,19 @@ class GenericFileSerializer(Serializer):
     attachment = FileField()
 
 
+class PhotoSerializer(GenericFileSerializer):
+    def create(self, ctx: OrderedDict):
+        ...
+
+
 class UploadFileView(CreateAPIView):
     def get_serializer_class(self) -> Type[BaseSerializer]:
-        return GenericFileSerializer
+        attachment: Union[InMemoryUploadedFile, None] = self.request.data.get('attachment')
+        if not attachment:
+            return Serializer
+
+        match attachment.content_type.split('/')[0]:
+            case 'image':
+                return PhotoSerializer
+            case _:
+                return GenericFileSerializer
