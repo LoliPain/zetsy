38857e589640cf69277d9e2e14f33447999eb8c4
LoliPain <p2love.loli@gmail.com>
2023-10-04 18:40:48 +0400

    use parser for view

--- server/api/views/upload.py
+++ server/api/views/upload.py
@@ -4,28 +4,29 @@ from rest_framework.generics import CreateAPIView
 from rest_framework.serializers import ModelSerializer, BaseSerializer, Serializer
 from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework.parsers import MultiPartParser
 
 from server.models.stored_file import FileModel
 
 
-class GenericFileSerializer(ModelSerializer):
+class PhotoSerializer(ModelSerializer):
     class Meta:
         model = FileModel
         fields = ("attachment",)
 
-
-class PhotoSerializer(GenericFileSerializer):
     def create(self, ctx: OrderedDict):
-        return FileModel(attachment=ctx['attachment'].name)
+        return FileModel(attachment=ctx['attachment'])
 
 
 class UploadFileView(CreateAPIView):
+    parser_classes = (MultiPartParser,)
+
     def __init__(self, **kwargs: Any):
         super().__init__(**kwargs)
         self.attachment = None
 
     def post(self, request: Request, *args: Any, **kwargs: Any) -> Response:
-        self.attachment = self.request.data.get('attachment')
+        self.attachment = self.request.FILES.get('attachment')
         if not self.attachment:
             return Response('No file provided')
         return super().post(request, *args, **kwargs)
