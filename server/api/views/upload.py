1f35e023971eb350dd48718b86c564f7eb62719b
LoliPain <p2love.loli@gmail.com>
2023-10-07 22:24:05 +0400

    serialize post request

--- server/api/views/upload.py
+++ server/api/views/upload.py
@@ -1,5 +1,6 @@
 from typing import OrderedDict, Type, Any
 
+from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 from rest_framework.generics import GenericAPIView
 from rest_framework.serializers import ModelSerializer, BaseSerializer
 from rest_framework.request import Request
@@ -14,6 +15,9 @@ class GenericFileSerializer(ModelSerializer):
         model = FileModel
         fields = ("attachment",)
 
+    def create(self, ctx: OrderedDict):
+        return FileModel(**ctx)
+
 
 class PhotoSerializer(GenericFileSerializer):
     def create(self, ctx: OrderedDict):
@@ -24,14 +28,16 @@ class UploadFileView(GenericAPIView):
     parser_classes = (MultiPartParser,)
 
     def post(self, request: Request, *args: Any, **kwargs: Any) -> Response:
-        if not self.request.FILES.get('attachment'):
-            return Response('No file provided')
-        # TODO: Use serializer
-        return Response()
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
 
     def get_serializer_class(self) -> Type[BaseSerializer]:
         _attachment = self.request.FILES.get('attachment')
-        match _attachment.content_type.split('/')[0]:
+        match _attachment.content_type.split('/')[0] if _attachment else None:
             case 'image':
                 return PhotoSerializer
             case _:
